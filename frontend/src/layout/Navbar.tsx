d79c3833ca25a52e627a0d3493ad9041b48973ec
eunmin-kim <53runtime@gmail.com>
2024-05-01 15:52:07 +0900

    [2024-05-01]Integrate TailwindCSS and refactor frontend components
    A major refactor was performed on the project's frontend. The App component was simplified and a Navbar component was introduced. In addition to this, TailwindCSS was incorporated into the project to streamline the styling process. Several unnecessary stylings and imports were also removed to tidy up the codebase.

---
+++ frontend/src/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-black text-white p-4 flex">
+            <h1 className="font-bold">CRUD with React, Nest</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+            </div>
+        </nav>
+    )
+}
